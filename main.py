b13bcac8cb457c0d3d89b28517a957d6fa60ce84
ryoma1108 <1108ryoma@gmail.com>
2026-06-29 11:58:04 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,15 +3,49 @@ import csv
 import requests
 from io import StringIO
 
+# =========================
+# 設定
+# =========================
 SHEET_CSV_URL = os.environ["SHEET_CSV_URL"]
+GAS_WEBHOOK_URL = os.environ["GAS_WEBHOOK_URL"]
 
+# =========================
+# CSV取得
+# =========================
 response = requests.get(SHEET_CSV_URL)
 response.raise_for_status()
 
-csv_text = response.text
-rows = list(csv.reader(StringIO(csv_text)))
+rows = list(csv.reader(StringIO(response.text)))
 
-print("読み込み成功")
-print("行数:", len(rows))
-print("先頭行:", rows[0])
-print("最新データ:", rows[1])
+# ヘッダーを除く
+data = rows[1:]
+
+# 最新行
+latest = data[0]
+
+# データ取得
+data_date = latest[1]
+nikkei = latest[2]
+per = latest[3]
+pbr = latest[4]
+eps = latest[5]
+dividend = latest[6]
+
+message = f"""📊 テスト
+
+データ日付：{data_date}
+
+日経平均：{nikkei}
+
+PER：{per}
+PBR：{pbr}
+EPS：{eps}
+
+配当利回り：{dividend}%"""
+
+# =========================
+# LINE送信
+# =========================
+response = requests.post(GAS_WEBHOOK_URL, data=message)
+
+print(response.text)
