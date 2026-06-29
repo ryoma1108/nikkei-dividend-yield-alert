4bec597d363d571e308d2b64362b58bfbd5fcb7f
ryoma1108 <1108ryoma@gmail.com>
2026-06-29 14:37:28 +0900

    Update state.py

--- state.py
+++ state.py
@@ -2,6 +2,18 @@ import requests
 from config import GAS_WEBHOOK_URL
 
 
+def get_state(key):
+    payload = {
+        "action": "get_state",
+        "key": key
+    }
+
+    response = requests.post(GAS_WEBHOOK_URL, json=payload)
+    response.raise_for_status()
+
+    return response.text.strip()
+
+
 def update_state(key, value):
     payload = {
         "action": "update_state",
